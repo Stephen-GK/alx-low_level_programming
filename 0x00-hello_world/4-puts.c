e2bb21882bc9fdf52dadc54ccb35a89da009a56c
Stephen-GK <gkstephen704@gmail.com>
2022-09-08 14:06:23 -0700

    Hello Printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *Main - entry point
+ *main - entry point
  *Description: 'describes the program'
- *return: 0
+ *Return: Always 0
  */
 
 int main(void)
@@ -11,4 +11,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
